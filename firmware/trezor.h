018d8430792dd391de5f9e8fcd577a97b3c74298
nining <nining@gmail.com>
2019-11-01 16:13:21 +0800

    bump version to 4.1.0

--- firmware/trezor.h
+++ firmware/trezor.h
@@ -21,8 +21,8 @@
 #define __TREZOR_H__
 
 #define VERSION_MAJOR 4
-#define VERSION_MINOR 0
-#define VERSION_PATCH 3
+#define VERSION_MINOR 1
+#define VERSION_PATCH 0
 
 #define STR(X) #X
 #define VERSTR(X) STR(X)
