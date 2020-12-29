5724b772e655391b8d72d5bb4fb90db242859500
nining <nining@gmail.com>
2020-12-29 15:32:12 +0800

    remove git update in build current code

--- build-current-firmware.sh
+++ build-current-firmware.sh
@@ -9,7 +9,6 @@ ELFFILE=build/bithd-$TAG.elf
 docker build -t $IMAGE .
 docker run -t -v "$(pwd)/build:/build:z" -v "$(pwd):/bithd-mcu" $IMAGE /bin/sh -c "\
 	cd /bithd-mcu && \
-	git submodule update --init && \
 	make -C vendor/libopencm3 && \
 	make -C vendor/nanopb/generator/proto && \
 	make -C firmware/protob && \
